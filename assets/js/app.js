0150cb4c316883b1506cac59b1d6abeb97f965d0
Germán Guerrero <87212199+GermanGuerrero95@users.noreply.github.com>
2023-08-23 00:38:56 -0500

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+const btns = document.querySelectorAll(".rates__btn");
+const textFinal = document.querySelector(".thankyouheader__p");
+
+btns[0].addEventListener("click", () => {
+  btns[0].classList.add("click");
+});
+btns[1].addEventListener("click", () => {
+  btns[1].classList.add("click");
+});
+btns[2].addEventListener("click", () => {
+  btns[2].classList.add("click");
+});
+btns[3].addEventListener("click", () => {
+  btns[3].classList.add("click");
+});
+btns[4].addEventListener("click", () => {
+  btns[4].classList.add("click");
+});
